8a319fba3c4628932b9152b164191d2cf2dca713
Diego Paredes <a20155800@pucp.edu.pe>
2021-12-09 12:40:54 -0500

    Nagevacion: 20% Interfacs: 100% Pruebas: 10%

---
+++ app/src/main/java/pe/edu/pucp/carpooling_pucp/ChoferViajes_AgregarRuta.kt
@@ -0,0 +1,11 @@
+package pe.edu.pucp.carpooling_pucp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChoferViajes_AgregarRuta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chofer_viajes_agregar_ruta)
+    }
+}
---
+++ app/src/main/java/pe/edu/pucp/carpooling_pucp/ChoferViajes_Registrar.kt
@@ -0,0 +1,11 @@
+package pe.edu.pucp.carpooling_pucp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChoferViajes_Registrar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chofer_viajes_registrar)
+    }
+}
