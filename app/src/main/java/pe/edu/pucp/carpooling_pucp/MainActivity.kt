8aefdf4521a82fc914c77b69d85b3e6632f6b7af
Diego Paredes <a20155800@pucp.edu.pe>
2021-12-09 21:39:55 -0500

    Nagevacion: 90% Interfacs: 100% Pruebas: 70% Conexion a servicios (BD) : DONE

--- app/src/main/java/pe/edu/pucp/carpooling_pucp/MainActivity.kt
+++ app/src/main/java/pe/edu/pucp/carpooling_pucp/MainActivity.kt
@@ -1,14 +1,26 @@
 package pe.edu.pucp.carpooling_pucp
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import pe.edu.pucp.carpooling_pucp.databinding.ActivityMainBinding
 import android.os.Bundle
-import android.view.Window
+import android.os.StrictMode
+import android.util.Log
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //Thread policy for internet access
+        val SDK_INT = android.os.Build.VERSION.SDK_INT
+        if (SDK_INT > 8) {
+            val policy = StrictMode.ThreadPolicy.Builder()
+                .permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
+
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -16,17 +28,21 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        binding.btLogin.setOnClickListener {
-            if (binding.txtUsername.text.toString() =="Chofer"){
-                val myIntent = Intent(this, ChoferViajes_Inicio::class.java)
-                startActivity(myIntent)
-            }else{
-                val myIntent = Intent(this, PasajeroViajes_distritos::class.java)
-                startActivity(myIntent)
+         binding.btLogin.setOnClickListener {
+        /*
+          //val connection = URL("http://10.0.2.2:8000/clientes/").openConnection() as HttpURLConnection
+          val connection = URL("http://10.0.2.2:8000/clientes/").readText()
+          Log.d("Conection",connection)
+          */
+             if (binding.txtUsername.text.toString() =="Chofer"){
+                 val myIntent = Intent(this, ChoferViajes_Inicio::class.java)
+                 startActivity(myIntent)
+             }else{
+                 val myIntent = Intent(this, PasajeroViajes_distritos::class.java)
+                 startActivity(myIntent)
+             }
+         }
 
-                /*val myIntent = Intent(this, PasajeroViajes_distritos::class.java)
-                startActivity(myIntent)*/
-            }
-        }
+
     }
 }
